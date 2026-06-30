9a415f16c47a84379ce34350cf94c46a664738b3
Pranav-Reddy29 <bunnygamingff29@gmail.com>
2026-06-30 12:10:15 +0530

    fix: update dynamic route params for Next.js 15

--- src/app/(main)/posts/[postId]/page.tsx
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -13,7 +13,9 @@ import { notFound } from "next/navigation";
 import { cache, Suspense } from "react";
 
 interface PageProps {
-  params: { postId: string };
+  params: Promise<{
+    postId: string;
+  }>;
 }
 
 const getPost = cache(async (postId: string, loggedInUserId: string) => {
@@ -30,8 +32,10 @@ const getPost = cache(async (postId: string, loggedInUserId: string) => {
 });
 
 export async function generateMetadata({
-  params: { postId },
+  params,
 }: PageProps): Promise<Metadata> {
+  const { postId } = await params;
+
   const { user } = await validateRequest();
 
   if (!user) return {};
@@ -43,7 +47,9 @@ export async function generateMetadata({
   };
 }
 
-export default async function Page({ params: { postId } }: PageProps) {
+export default async function Page({ params }: PageProps) {
+  const { postId } = await params;
+
   const { user } = await validateRequest();
 
   if (!user) {
@@ -61,6 +67,7 @@ export default async function Page({ params: { postId } }: PageProps) {
       <div className="w-full min-w-0 space-y-5">
         <Post post={post} />
       </div>
+
       <div className="sticky top-[5.25rem] hidden h-fit w-80 flex-none lg:block">
         <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
           <UserInfoSidebar user={post.user} />
@@ -82,27 +89,32 @@ async function UserInfoSidebar({ user }: UserInfoSidebarProps) {
   return (
     <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="text-xl font-bold">About this user</div>
+
       <UserTooltip user={user}>
         <Link
           href={`/users/${user.username}`}
           className="flex items-center gap-3"
         >
           <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+
           <div>
             <p className="line-clamp-1 break-all font-semibold hover:underline">
               {user.displayName}
             </p>
+
             <p className="line-clamp-1 break-all text-muted-foreground">
               @{user.username}
             </p>
           </div>
         </Link>
       </UserTooltip>
+
       <Linkify>
         <div className="line-clamp-6 whitespace-pre-line break-words text-muted-foreground">
           {user.bio}
         </div>
       </Linkify>
+
       {user.id !== loggedInUser.id && (
         <FollowButton
           userId={user.id}
